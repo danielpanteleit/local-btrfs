b65ad7cedc0b9069912b3d121a9ef050987aea1e
Daniel Panteleit <panteleit@cs.uni-bonn.de>
2017-03-29 15:41:38 +0200

    tests did not run

--- scripts/test-in-docker.sh
+++ scripts/test-in-docker.sh
@@ -5,4 +5,6 @@ set -eux
 dd if=/dev/zero of=/btrfs.img bs=1M count=0 seek=100
 mkfs.btrfs /btrfs.img
 mount -o loop /btrfs.img /btrfs
-go test -v .
+
+PKGS=$(find . -type f -name '*.go' | sed -r 's|/[^/]+$||' |sort |uniq | grep -v "^./vendor")
+go test -v $PKGS
