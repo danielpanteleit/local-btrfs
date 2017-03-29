a7e17ec5fd800f5bd8aacda09e51fcc8dcdef09b
Daniel Panteleit <panteleit@cs.uni-bonn.de>
2017-03-29 14:01:16 +0200

    removed build-arg

--- scripts/docker-test.sh
+++ scripts/docker-test.sh
@@ -2,5 +2,5 @@
 
 set -eux
 
-docker build --build-arg=http_proxy -f Dockerfile-test -t local-btrfs-test .
+docker build -f Dockerfile-test -t local-btrfs-test .
 docker run --privileged --rm -v `pwd`:/go/src/local-btrfs:ro local-btrfs-test
